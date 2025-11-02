8683499fd0929d9918a450e84ce9060598f6e782
Samee Alam <sameealam50@gmail.com>
2025-11-02 18:37:47 +0530

    frontend url is added inside the backend

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 // ✅ Include all frontend origins
 const allowedOrigins = process.env.CORS_ORIGIN
   ? process.env.CORS_ORIGIN.split(",")
-  : ["http://localhost:5173", "http://localhost:5174"];
+  : ["https://ai-powered-e-commerce-website-frontend-0e3m.onrender.com", "http://localhost:5174"];
 
 const corsOptions = {
   origin: (origin, callback) => {
