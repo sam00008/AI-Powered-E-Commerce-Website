1abb37dc2d459e424b9a641006ec46e3a546f9e3
Samee Alam <sameealam50@gmail.com>
2025-11-02 20:59:44 +0530

    error fix

--- Backend/app.js
+++ Backend/app.js
@@ -36,7 +36,7 @@ import authRouter from "./routes/auth.routes.js";
 import productRouter from "./routes/product.routes.js";
 import orderRouter from "./routes/order.routes.js";
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/product", productRouter);
+app.use("/api/product", productRouter);
 app.use("/api/v1/order", orderRouter);
 
 // Default route
